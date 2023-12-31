12593673fac23d1e4b7e33e3dc1393456a035e9e
vinny-888 <>
2023-12-31 09:22:00 -0800

    update snapshot

--- injectedScript.js
+++ injectedScript.js
@@ -27,16 +27,12 @@ function buildItemExt(itemName, items, citiesStr){
         <div style="background-color:white;z-index:1000;padding:0px;border: 1px solid #fff;">
             <div style="background-color: #1f2932;width: 100%;height: 24px;padding:4px;">
                 <div id="refreshBtn" style="font-weight: bold;text-align: center;color: #000;cursor: pointer;float:left;background-color: #00c5ff;margin-left: 6px;">
-                    Refresh Counts
+                    Refresh page to load counts
                 </div>
                 <div id="closeBtn" style="font-weight: bold;text-align: center;color: #F00;width: 20px;height: 20px;cursor: pointer;float:right;">
                     X
                 </div>
             </div>
-            <div id="iframe_div" style="width: 520px;height: 600px;">
-            <iframe style="display: block;width:100%;height: 100%;" src="https://vinny-888.github.io/LiquidLandsThematicMaps/items/item_small.html?item=${itemName}&items=${items}&cities=${citiesStr}"></iframe>
-            </div>
-            
         </div>`;
     headups.style.cssText = `position:fixed;top:70px;right:10px;z-index: 9999;`;
     body.appendChild(headups);
